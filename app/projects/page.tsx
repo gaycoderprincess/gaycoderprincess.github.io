ed1f4e725c63726309fec11a211740361a734ed2
💖 Chloe 💖 <gaycoderprincess@proton.me>
2024-12-15 15:42:42 +0100

    default projects page to newest first

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,5 +1,5 @@
 import ProjectsPage from "@/app/projects/actualPage";
 
 export default async function Projects() {
-    return ProjectsPage((a, b) => a.metadata.name.localeCompare(b.metadata.name))
+    return ProjectsPage((a, b) => (new Date(b.metadata.lastUpdated)).getTime() - (new Date(a.metadata.lastUpdated)).getTime())
 }
